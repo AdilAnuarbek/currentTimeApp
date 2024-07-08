7053798084e22a42229a1fc259da1b65ff5f22a9
Adil Anuarbek <adilanuarbek3@gmail.com>
2024-07-08 11:06:18 +0500

    added updateTime and getTimezone, added webdevserver tool

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-timeContainer = document.getElementById("time");
+import dayjs from 'dayjs/esm/index.js';
+import utc from 'dayjs/esm/plugin/utc/index.js';
+import timezone from 'dayjs/esm/plugin/timezone/index.js';
 
-timeContainer.innerHTML = 'Hello';
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+
+const updateTime = () => {
+    const current_time = `Current time is ${dayjs().format('MMMM D, YYYY hh:mm:ss A')}`;
+    const timeContainer = document.getElementById("time");
+    timeContainer.innerHTML = current_time;
+}
+
+const getTimezone = () => {
+    const timeZoneContainer = document.getElementById("time-zone");
+    timeZoneContainer.innerHTML = dayjs.tz.guess();
+}
+
+
+updateTime();
+setInterval(updateTime, 1000);
+getTimezone();
